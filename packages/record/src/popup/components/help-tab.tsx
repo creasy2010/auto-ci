b71978644e37f912feda0974a7c5d281f7297552
yangxiaodong <yangxiaodong@qianmi.com>
2019-08-26 22:50:00 +0800

    add: 添加进来..

--- packages/record/src/popup/components/help-tab.tsx
+++ packages/record/src/popup/components/help-tab.tsx
@@ -7,39 +7,92 @@
  * @Date    2019/8/26
  **/
 
+import * as React from 'react';
 
-
-import  * as React from 'react';
-import PropTypes from 'prop-types';
-
-
-interface IHelpTabP{
-  [name:string]:any;
+interface IHelpTabP {
+  [name: string]: any;
 }
 
-interface IHelpTabS{
-  [name:string]:any;
+interface IHelpTabS {
+  [name: string]: any;
 }
 
-export default class HelpTab extends React.Component<IHelpTabP,IHelpTabS> {
-  static defaultProps = {
+export default class HelpTab extends React.Component<IHelpTabP, IHelpTabS> {
+  static defaultProps = {};
 
-  };
- 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
-  componentDidMount() {
-  }
-  
+  componentDidMount() {}
+
   render() {
     return (
-      <div></div>
+      <div className="tab help-tab">
+        <div className="content">
+          <h4>Recording</h4>
+          <ul>
+            <li>To start recording hit <strong>Record</strong></li>
+            <li>
+              Hit <kbd>tab</kbd> after you finish typing in an <kbd>input</kbd>{' '}
+              element.
+            </li>
+            <li>Click links, inputs and other elements.</li>
+            <li>
+              Wait for full page load on each navigation. The icon will switch
+              from <img src="/images/icon_rec.png" /> to
+              <img src="/images/icon_wait.png" />.
+            </li>
+            <li>
+              Click <strong>Pause</strong> when you want to navigate without
+              recording anything.
+              Hit <strong>Resume</strong> to continue recording.
+            </li>
+          </ul>
+          <h4>Controls</h4>
+          <p>
+            While recording, right click to show extra controls that trigger
+            various functions like recording
+            screenshots.
+            <img
+              src="/images/context_menu.png"
+              alt="context menu"
+              className="w-100"
+            />
+          </p>
+          <h4>Keyboard shortcuts</h4>
+          <ul>
+            <li>Take screenshot: Ctrl+Shift+A</li>
+            <li>Take clipped screenshot: Ctrl+Shift+M</li>
+          </ul>
+          <p>
+            For more help and examples,{' '}
+            <a href="https://checklyhq.com/puppeteer-recorder" target="_blank">
+              go to the
+              help docs
+            </a>
+          </p>
+          <h4>Replaying</h4>
+          <p>
+            Install{' '}
+            <a href="https://github.com/GoogleChrome/puppeteer">Puppeteer</a> on
+            your machine. Copy and paste
+            the code
+            into a file and run as a standard node program
+          </p>
+          <pre>
+            npm install puppeteer
+            node my-script.js
+          </pre>
+        </div>
+        <div className="help-footer text-muted">
+          sponsored by
+          <a href="https://checklyhq.com" target="_blank">
+            <img src="/images/text_racoon_logo.svg" alt="" />
+          </a>
+        </div>
+      </div>
     );
   }
 }
-
- 
--- packages/record/src/popup/components/recording-tab.tsx
+++ packages/record/src/popup/components/recording-tab.tsx
@@ -6,36 +6,81 @@
  * @coder.yang2010@gmail.com
  * @Date    2019/8/26
  **/
-import  * as React from 'react';
+import * as React from 'react';
 
-
-interface IRecordingTabP{
-  [name:string]:any;
+interface IRecordingTabP {
+  isRecording: boolean;
+  liveEvents: any[];
+  [name: string]: any;
 }
 
-interface IRecordingTabS{
-  [name:string]:any;
+interface IRecordingTabS {
+  [name: string]: any;
 }
 
-export default class RecordingTab extends React.Component<IRecordingTabP,IRecordingTabS> {
-  static defaultProps = {
+export default class RecordingTab extends React.Component<
+  IRecordingTabP,
+  IRecordingTabS
+> {
+  static defaultProps = {};
 
-  };
- 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
-  componentDidMount() {
-  }
-  
+  componentDidMount() {}
+
   render() {
     return (
-      <div></div>
+      <div className="tab recording-tab">
+        <div className="content">
+          {this.props.isRecording
+            ? <div className="events" v-show="isRecording">
+                <p
+                  className="text-muted text-center loading"
+                  v-show="liveEvents.length === 0"
+                >
+                  Waiting for events
+                </p>
+                <ul className="event-list">
+
+                  {this.props.liveEvents.map((event, index) =>
+                    <li className="event-list-item">
+                      <div className="event-label">
+                        {index + 1}
+                      </div>
+                      <div className="event-description">
+                        <div className="event-action">{event.action}</div>
+                        <div className="event-props text-muted">
+                          {event.selector || this.parseEventValue(event)} :{event.value}{' '}
+                        </div>
+                      </div>
+                    </li>,
+                  )}
+                </ul>
+              </div>
+            : <div className="empty">
+                <img src="/images/Desert.svg" alt="desert" width="78px" />
+                <h3>No recorded events yet</h3>
+                <p className="text-muted">Click record to begin</p>
+                <div className="nag-cta">
+                  <a
+                    href="https://surveys.hotjar.com/s?siteId=717179&surveyId=137462"
+                    target="_blank"
+                  >
+                    ⚡️ Puppeteer Recorder Pro is coming →
+                  </a>
+                </div>
+              </div>}
+        </div>
+      </div>
     );
   }
+  parseEventValue = event => {
+    if (event.action === 'viewport*')
+      return `width: ${event.value.width}, height: ${event.value.height}`;
+    if (event.action === 'goto*') return event.href;
+    if (event.action === 'navigation*') return '';
+  };
 }
-
- 
