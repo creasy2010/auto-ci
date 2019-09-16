00bb206853cffe49d4dd7101e4193a3505d09ebd
杨晓东 <coder.yang2010@gmail.com>
2019-09-16 15:22:49 +0800

    添加一个用例测试project:${project}下scene[${scene}]fileName${fileName}

---
+++ packages/projects/supplier/scene/use-case/uc-demo140925.js
@@ -0,0 +1,11 @@
+module.exports =   {
+  desc:"TODO 要添加进来. ",
+  id:"uniqueId",
+  async exec(
+    {page},
+    {sleep, log, screenshot, waitElementVisiable},
+  ) {
+     
+  },
+  neworkMock :undefined
+};
