e9807d7593bac14d772c1462ece505c9501e77f2
yangxiaodong <yangxiaodong@qianmi.com>
2019-09-17 11:25:38 +0800

    add: ts define

---
+++ packages/projects/supplier/common/login.js
@@ -0,0 +1,98 @@
+/**
+ * @desc
+ *
+ * @使用场景
+ *
+ * @coder.yang2010@gmail.com
+ * @Date    2019/8/22
+ **/
+module.exports={
+  desc:"登录",
+  id:"78979",
+  async exec(
+    {page},
+    {sleep, log, screenshot, waitElementVisiable},
+  ) {
+
+    await page.evaluate(`window.__testContext__.goto('/login')`);
+    // await page.setViewport({width: 1440, height: 745});
+
+    await sleep(0.5);
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.click('.ant-row #input-form_account');
+
+    await sleep(0.5);
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '1');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '7');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '7');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '0');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '0');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '0');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '0');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '0');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '0');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '0');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.type('.ant-row #input-form_account', '0');
+
+    await page.waitForSelector('.ant-row #input-form_account');
+    await page.click('.ant-row #input-form_account');
+
+    await sleep(0.5);
+    await page.waitForSelector(
+      '.form-box > .ant-form > .ant-row:nth-child(1) > .ant-col > .ant-form-item-control',
+    );
+    await page.click(
+      '.form-box > .ant-form > .ant-row:nth-child(1) > .ant-col > .ant-form-item-control',
+    );
+
+    await sleep(0.5);
+    await page.waitForSelector('.ant-row #input-form_password');
+    await page.click('.ant-row #input-form_password');
+
+    await sleep(0.5);
+    await page.waitForSelector('.ant-row #input-form_password');
+    await page.type('.ant-row #input-form_password', '2');
+
+    await page.waitForSelector('.ant-row #input-form_password');
+    await page.type('.ant-row #input-form_password', '2');
+
+    await page.waitForSelector('.ant-row #input-form_password');
+    await page.type('.ant-row #input-form_password', '2');
+
+    await page.waitForSelector('.ant-row #input-form_password');
+    await page.type('.ant-row #input-form_password', '2');
+
+    await page.waitForSelector('.ant-row #input-form_password');
+    await page.type('.ant-row #input-form_password', '2');
+
+    await page.waitForSelector('.ant-row #input-form_password');
+    await page.type('.ant-row #input-form_password', '2');
+
+    await page.waitForSelector(
+      '.login > .right-login-info > .form-box > .ant-form > .btn',
+    );
+    await page.click('.login > .right-login-info > .form-box > .ant-form > .btn');
+
+  }
+}
---
+++ packages/projects/supplier/scene/use-case/config/config.js
@@ -0,0 +1,20 @@
+/**
+ * @desc
+ *
+ * @使用场景
+ *
+ * @coder.yang2010@gmail.com
+ * @Date    2019/9/12
+ **/
+
+export default {
+  before:({page})=>{
+
+  },
+  beforeUseCases:["common/login.js"],
+
+  after:({page})=>{
+
+  },
+  afterUseCases:[]
+}
